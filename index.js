b872373b745e9ecc0ace94fde86665ffac704b0f
aniketk2112 <123199710+aniketk2112@users.noreply.github.com>
2023-02-05 18:53:39 -0500

    parsedUrl

---
+++ index.js
@@ -0,0 +1,31 @@
+/*
+main file
+*/
+
+//dependencies
+var http=require('http')
+var url=require('url')
+
+
+//create server
+var server=http.createServer(function(req,res){
+
+	// get the url and parse it
+	var parsedUrl=url.parse(req.url,true);
+
+	//get the path
+	var path=parsedUrl.pathname;
+	var trimmedPath=path.replace(/^\/+|\/+$/g,'');
+
+	//send the response
+	res.end('Hello World');
+	
+	//log the request path
+	console.log('request recieved on path:'+trimmedPath)
+})
+
+
+//asign port for a server
+server.listen(3000,function(){
+console.log('Listening on port 3000!')
+})
