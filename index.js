5f0ee8705684127931011ad7f485b9797fcad760
Aniket <aniketkanurkar@icloud.com>
2023-02-06 21:25:02 -0500

    routingRequests

--- index.js
+++ index.js
@@ -36,13 +36,37 @@ var server=http.createServer(function(req,res){
 	req.on('end',function(){
 		buffer+=decoder.end();
 
-	//send the response
-	res.end('Hello World');
-	
-	//log the request path
-	console.log('Request recieved with this payload :',buffer)
-	});
-	
+		//choose the handler this request should go.
+		var chooseHandler=typeof(router[trimmedPath])!=='undefined' ? router[trimmedPath]:handlers.notFound
+
+		//construct the data object to send to the handler
+		var data={
+			'trimmedPath':trimmedPath,
+			'queryStringObject':queryStringObject,
+			'method':method,
+			'headers':headers,
+			'payload':buffer
+		}
+
+		//route the request to the handler specified in the router
+		chooseHandler(data,function(statusCode,payload){
+			//use the statuscode calles back to the handler, or default to 200
+			statusCode=typeof(statusCode)=='number' ? statusCode:200
+
+			//use the payload called back to the handler, or defaulto to an empty object
+			payload=typeof(payload)=='object' ? payload:{};
+
+			//Convert the payload to a string
+			var payloadString=JSON.stringify(payload);
+
+			//return the response
+			res.writeHead(statusCode);
+			res.end(payloadString);
+
+			//log the request path
+			console.log('Returning this response :',statusCode,payloadString)
+		});
+		});
 });
 
 
@@ -50,3 +74,25 @@ var server=http.createServer(function(req,res){
 server.listen(3000,function(){
 console.log('Listening on port 3000!')
 })
+
+//Define the handler
+var handlers={};
+
+//sample handler
+handlers.sample=function(data,callback){
+	//callback a http status code, and a payload object
+	callback(406,{'name':'sample handler'});
+
+}
+
+//Not found handler
+handlers.notFound=function(data,callback){
+	callback(404);
+}
+
+
+//Define a request router
+var router={
+'sample':handlers.sample
+};
+
